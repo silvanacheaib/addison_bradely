b60f18dbc1a8801ac23bb83bfd14c8971ac684ac
silvanacheaib <silvanacheaib@gmail.com>
2025-09-30 11:33:56 +0300

    Design Package Update

---
+++ Scripts/script.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const searchIcon = document.getElementById('searchIcon');
+    const searchBoxContainer = document.getElementById('searchBoxContainer');
+    if (searchIcon) {
+        searchIcon.addEventListener('click', function() {
+            if(searchBoxContainer){
+                searchBoxContainer.classList.add('visible');
+            }
+        });
+    }
+});
