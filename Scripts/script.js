ad1a97896c6d06f388024429f3917f6640c4ce5f
silvanacheaib <silvanacheaib@gmail.com>
2025-10-11 09:38:11 +0300

    Script adjustment

--- Scripts/script.js
+++ Scripts/script.js
@@ -1,14 +1,14 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const searchIcon = document.getElementById('searchIcon');
-    const searchBoxContainer = document.getElementById('searchBoxContainer');
-    if (searchIcon) {
-        searchIcon.addEventListener('click', function() {
-            if(searchBoxContainer){
-                searchBoxContainer.classList.add('visible');
-            }
-        });
-    }
-});
+// document.addEventListener('DOMContentLoaded', function() {
+//     const searchIcon = document.getElementById('searchIcon');
+//     const searchBoxContainer = document.getElementById('searchBoxContainer');
+//     if (searchIcon) {
+//         searchIcon.addEventListener('click', function() {
+//             if(searchBoxContainer){
+//                 searchBoxContainer.classList.add('visible');
+//             }
+//         });
+//     }
+// });
 // jQuery(document).ready(function($){
 //     $('.awb-swiper-carousel').each(function(){
 //         var swiper = this.swiper; // get Swiper instance
